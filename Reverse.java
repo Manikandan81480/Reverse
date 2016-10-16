dd0ceeb68411c38df45cce455f61552e4fb66a82
Manikandan81480 <mani81480@gmail.com>
2016-10-16 03:47:14 -0700

    Update and rename README.md to Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,9 @@
+class Reverse
+{
+public static void main(String... args)
+{
+Scanner x=new Scanner(System.in);
+StringBuffer s=x.nextLine();
+System.out.println("The Reverse String"+s.reverse());
+}
+}
